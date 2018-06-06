fa185bc029dd22e6eee9abd703e78a7e298173d0
Travis Robson <travis.robson@ligo.org>
2018-06-05 20:02:51 -0600

    huh

--- src/EccBinary.cpp
+++ src/EccBinary.cpp
@@ -7,23 +7,28 @@
 
 #include <cmath>
 
-
 #include "EccBinary.h"
 
 //namespace eb {
 
-EccBinary::EccBinary(double m1, double m2, double e0, double F0) {
+EccBinary::EccBinary(double m1, double m2, double et0, double F0) {
 	// TODO Auto-generated constructor stub
-	this->m1 = m1;
-	this->m2 = m2;
-	this->e0 = e0;
-	this->F0 = F0;
+	this->m1  = m1;
+	this->m2  = m2;
+	this->et0 = et0;
+	this->F0  = F0;
 
 	calc_mass_parameters();
+
+	this->orb = new Orbit();
+	this->orb->set_N(10);    // dummy for now to make sure I coded things properly
+
 }
 
 EccBinary::~EccBinary() {
 	// TODO Auto-generated destructor stub
+
+	delete orb;
 }
 
 void EccBinary::calc_m()
--- src/test_EccBinary.cpp
+++ src/test_EccBinary.cpp
@@ -20,21 +20,23 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	double m1, m2;
-	double e0, F0;
+	double m1,  m2;
+	double et0, F0;
 
 	cout << "\n==================================================\n" << endl;
 
-	m1 = 10.0*TSUN;
-	m2 = 10.0*TSUN;
-	e0 = 0.1;
-	F0 = 5.0;
+	m1  = 10.0*TSUN;
+	m2  = 10.0*TSUN;
+	et0 = 0.1;
+	F0  = 5.0;
 
-	EccBinary *eb = new EccBinary(m1, m2, e0, F0);
+	EccBinary *eb = new EccBinary(m1, m2, et0, F0);
 
-	cout << "m.......... " << eb->get_m()/TSUN << " MSUN"  << endl;
-	cout << "eta........ " << eb->get_eta()         	   << endl;
-	cout << "Mc......... " << eb->get_Mc()/TSUN << " MSUN" << endl;
+	cout << "m............ " << eb->get_m()/TSUN << " MSUN"  << endl;
+	cout << "eta.......... " << eb->get_eta()         	   << endl;
+	cout << "Mc........... " << eb->get_Mc()/TSUN << " MSUN" << endl << endl;
+
+	cout << "N for soln... " << eb->orb->get_N() << endl;
 
 
 	cout << "\n==================================================\n" << endl;
